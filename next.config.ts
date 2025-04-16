f2d217b6e5cf908ec7d2efb38ad98ca154a45762
TayyabKM <tayyabirfan05@gmail.com>
2025-04-16 18:31:56 +0500

    Next Config File Update

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export", // Keep this for static export
   images: {
-    unoptimized: true, // ✅ Disable image optimization for static export
+    unoptimized: true, // Optional, only if you're using basic <img> tags instead of <Image />
   },
 };
 
