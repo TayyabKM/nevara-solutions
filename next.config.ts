fabe5312ab2cb0ea4f59078ca7eba43de56a0d48
anakin-dabir <anakindabir@gmail.com>
2025-04-25 15:24:57 +0500

    Fix: Forcing functions to build dynamically

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export", // Keep this for static export
+  output: "standalone",
   images: {
     unoptimized: true, // ✅ Disable image optimization for static export
   },
--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -3,6 +3,8 @@
 import { NextResponse } from "next/server";
 import nodemailer from "nodemailer";
 
+export const dynamic = "force-dynamic";
+
 export async function POST(req: Request) {
   try {
     const body = await req.json();
